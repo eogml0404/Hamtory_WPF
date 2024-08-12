0c6d8e390aa7d32656d9aa22c53360d250726c87
cocoachou <123175470+cocoachou@users.noreply.github.com>
2024-08-12 19:28:43 +0900

    데이터 비동기 호출

--- Hamtory_WPF/ProcessData.cs
+++ Hamtory_WPF/ProcessData.cs
@@ -9,17 +9,18 @@ namespace Hamtory_WPF
 {
     public class ProcessData
     {
-        public List<DataValues> LoadDataFile(string filePath)
+        public async Task<List<DataValues>> LoadDataFileAsync(string filePath)
         {
             List<DataValues> dataList = new List<DataValues>();
 
+            // StreamReader를 비동기로 사용
             using (var data = new StreamReader(filePath))
             {
                 bool firstLine = true;
 
                 while (!data.EndOfStream)
                 {
-                    var row = data.ReadLine();
+                    var row = await data.ReadLineAsync(); // 비동기적으로 한 줄 읽기
 
                     if (firstLine)
                     {
@@ -29,9 +30,15 @@ namespace Hamtory_WPF
 
                     var columns = row.Split(',');
 
-
-                    dataList.Add(new DataValues(DateTime.Parse(columns[0]), int.Parse(columns[1]), int.Parse(columns[2]), int.Parse(columns[3]), int.Parse(columns[4]) , double.Parse(columns[5]), columns[6]));
-                    
+                    // 데이터 파싱 후 리스트에 추가
+                    dataList.Add(new DataValues(
+                        DateTime.Parse(columns[0]),
+                        int.Parse(columns[1]),
+                        int.Parse(columns[2]),
+                        int.Parse(columns[3]),
+                        int.Parse(columns[4]),
+                        double.Parse(columns[5]),
+                        columns[6]));
                 }
             }
 
--- Hamtory_WPF/dataList.cs
+++ Hamtory_WPF/dataList.cs
@@ -6,6 +6,9 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using static System.Runtime.InteropServices.JavaScript.JSType;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
 
 namespace Hamtory_WPF
 {
@@ -13,12 +16,11 @@ namespace Hamtory_WPF
     {
         private static ProcessData dataLoader = new ProcessData();
         public static List<DataValues> datas { get; private set; }
-        static DataList()
+
+        // 비동기 초기화 메서드
+        public static async Task InitializeAsync()
         {
-            datas = dataLoader.LoadDataFile("melting_tank.csv");
-            
+            datas = await dataLoader.LoadDataFileAsync("melting_tank.csv");
         }
-
     }
-
 }
