448f60ac7a8be5faf3211a0eec089ac9ac043260
eogml0404 <eogml0404@naver.com>
2024-08-12 09:49:55 +0900

    test

--- Hamtory_WPF/OpChart.xaml.cs
+++ Hamtory_WPF/OpChart.xaml.cs
@@ -1,10 +1,22 @@
-﻿using LiveCharts;
-using LiveCharts.Wpf;
-using LiveCharts.Defaults;
-using System;
+﻿using System;
 using System.Collections.Generic;
+using System.Globalization;
+using System.IO;
 using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
 using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using CsvHelper;
+using LiveCharts;
+using LiveCharts.Wpf;
 
 namespace Hamtory_WPF
 {
@@ -13,70 +25,29 @@ namespace Hamtory_WPF
         public OpChart()
         {
             InitializeComponent();
-            InitializeChart();
+            // Initialize the chart with default settings if needed
         }
 
-        private void InitializeChart()
+        public void SetData(ChartValues<double> meltTempData,
+                            ChartValues<double> motorSpeedData,
+                            ChartValues<double> meltWeightData,
+                            ChartValues<double> inspData)
         {
-            // X축과 Y축 둘 다 줌과 팬을 가능하도록 설정
-            cartesianChart.Zoom = ZoomingOptions.Xy;  // X축과 Y축 모두 줌 가능
-            cartesianChart.Pan = PanningOptions.Xy;   // X축과 Y축 모두 팬 가능
-        }
-
-        public void SetData(List<DateTime> times,
-                            List<double> meltTempData,
-                            List<double> motorSpeedData,
-                            List<double> meltWeightData,
-                            List<double> inspData)
-        {
-            // Melt Temp Series 설정
-            meltTempSeries.Values = new ChartValues<ObservablePoint>(
-                meltTempData.Select((value, index) => new ObservablePoint(times[index].Ticks, value))
-            );
-            meltTempSeries.LabelPoint = point => $"{point.Y:F0}"; // 소수점 없이 정수로 표시
-
-            // Motor Speed Series 설정
-            motorSpeedSeries.Values = new ChartValues<ObservablePoint>(
-                motorSpeedData.Select((value, index) => new ObservablePoint(times[index].Ticks, value))
-            );
-            motorSpeedSeries.LabelPoint = point => $"{point.Y:F0}"; // 소수점 없이 정수로 표시
-
-            // Melt Weight Series 설정
-            meltWeightSeries.Values = new ChartValues<ObservablePoint>(
-                meltWeightData.Select((value, index) => new ObservablePoint(times[index].Ticks, value))
-            );
-            meltWeightSeries.LabelPoint = point => $"{point.Y:F0}"; // 소수점 없이 정수로 표시
-
-            // INSP Series 설정
-            inspSeries.Values = new ChartValues<ObservablePoint>(
-                inspData.Select((value, index) => new ObservablePoint(times[index].Ticks, value))
-            );
-            inspSeries.LabelPoint = point => $"{point.Y:F2}"; // 소수점 둘째 자리까지 표시
-
-            // X축 포맷 설정
-            cartesianChart.AxisX[0].LabelFormatter = value => new DateTime((long)value).ToString("HH:mm");
-            cartesianChart.AxisY[0].LabelFormatter = value => value.ToString("F0"); // Y축 값 소수점 없이 표시
+            meltTempSeries.Values = meltTempData;
+            motorSpeedSeries.Values = motorSpeedData;
+            meltWeightSeries.Values = meltWeightData;
+            inspSeries.Values = inspData;
         }
 
         public void LoadSampleData()
         {
-            var now = DateTime.Now;
+            // Example of sample data
+            var meltTempData = new ChartValues<double> { 1, 2, 3, 4, 5 };
+            var motorSpeedData = new ChartValues<double> { 5, 4, 3, 2, 1 };
+            var meltWeightData = new ChartValues<double> { 2, 3, 4, 5, 6 };
+            var inspData = new ChartValues<double> { 6, 5, 4, 3, 2 };
 
-            var times = new List<DateTime>
-            {
-                now,
-                now.AddMinutes(30),
-                now.AddMinutes(60),
-                now.AddMinutes(90),
-                now.AddMinutes(120)
-            };
-
-            var meltTempData = new List<double> { 1, 2, 3, 4, 5 };
-            var motorSpeedData = new List<double> { 5, 4, 3, 2, 1 };
-            var meltWeightData = new List<double> { 2, 3, 4, 5, 6 };
-            var inspData = new List<double> { 6, 5, 4, 3, 2 };
-
-            SetData(times, meltTempData, motorSpeedData, meltWeightData, inspData);
+            SetData(meltTempData, motorSpeedData, meltWeightData, inspData);
         }
     }
 }
